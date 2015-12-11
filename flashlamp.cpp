5248055d9cafbae3e954acac6bd7ec1294a1308c
mongondum <mongondum@gmail.com>
2015-12-11 23:57:49 +0700

    update

--- flashlamp.cpp
+++ flashlamp.cpp
@@ -13,6 +13,7 @@ flashlamp::flashlamp(QWidget *parent) :
 
     connect(client, SIGNAL(connected()), this, SLOT(onMQTT_Connected()));
     connect(client, SIGNAL(disconnected()), this, SLOT(onMQTT_disconnected()));
+    connect(client, SIGNAL(received(QMQTT::Message)),this, SLOT(onMQTT_Received(QMQTT::Message)));
 
     mqtt_setup();
 
@@ -30,30 +31,22 @@ flashlamp::~flashlamp()
 void flashlamp::on_Base_Dial_sliderMoved(int position)
 {
     ui->base_lcd_number->display(position);
-    long y=map(position,0,360,8,25);
-    ui->base_pulse_label->setText(QString::number(y));
-    mqtt_pub(mqtt_topic_base,QString::number(y));
-
+    mqtt_pub(mqtt_topic_base,QString::number(position));
 
 }
 
-void flashlamp::on_Holder_Dial_sliderMoved(int position)
+void flashlamp::on_Arm_Dial_sliderMoved(int position)
 {
     ui->arm_lcd_number->display(position);
-    long y=map(position,0,180,8,25);
-    ui->arm_pulse_label->setText(QString::number(y));
-    mqtt_pub(mqtt_topic_arm,QString::number(y));
+    mqtt_pub(mqtt_topic_arm,QString::number(position));
 }
 void flashlamp::mqtt_setup()
 {
     try{
-        client->setHost("localhost");
-        client->setPort(1883);
+        client->setHost("192.168.43.137");
+        client->setPort(1884);
         //client->setUsername
         client->connect();
-
-        QMQTT::Message msg(0,"/myconfix/openqt","Hello Flash Lamp");
-        client->publish(msg);
 
     }catch (...){
 
@@ -62,6 +55,10 @@ void flashlamp::mqtt_setup()
 void flashlamp::onMQTT_Connected()
 {
     qDebug() << "Connected";
+
+    QMQTT::Message msg(0,"/openqt","Hello Flash Lamp PC");
+    client->publish(msg);
+    mqtt_sub(mqtt_topic_sub);
 }
 void flashlamp::onMQTT_disconnected()
 {
@@ -72,9 +69,30 @@ void flashlamp::mqtt_pub(QString topic, QString value)
         QMQTT::Message msg(0,topic,value.toUtf8());
         client->publish(msg);
 }
-
-long flashlamp::map(long x, long in_min, long in_max, long out_min, long out_max)
+void flashlamp::mqtt_sub(QString topic)
 {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    client->subscribe(topic,0);
+}
+void flashlamp::onMQTT_Received(const QMQTT::Message &message)
+{
+    qDebug() <<"Topic : " << message.topic();
+    qDebug() <<"Value : " << message.payload().toInt();
+
+    if(message.topic() == mqtt_topic_base){
+        ui->Base_Dial->setValue(message.payload().toInt());
+    }
+    if(message.topic() == mqtt_topic_arm){
+        ui->Arm_Dial->setValue(message.payload().toInt());
+    }
 }
 
+
+void flashlamp::on_Arm_Dial_valueChanged(int value)
+{
+     ui->arm_lcd_number->display(value);
+}
+
+void flashlamp::on_Base_Dial_valueChanged(int value)
+{
+    ui->base_lcd_number->display(value);
+}
--- flashlamp.h
+++ flashlamp.h
@@ -20,21 +20,27 @@ public:
 public slots:
     void onMQTT_Connected();
     void onMQTT_disconnected();
+    void onMQTT_Received(const QMQTT::Message &message);
     void mqtt_setup();
     void mqtt_pub(QString topic,QString value);
+    void mqtt_sub(QString topic);
 
 private slots:
 
     void on_Base_Dial_sliderMoved(int position);
 
-    void on_Holder_Dial_sliderMoved(int position);
+    void on_Arm_Dial_sliderMoved(int position);
+
+    void on_Arm_Dial_valueChanged(int value);
+
+    void on_Base_Dial_valueChanged(int value);
 
 private:
     Ui::flashlamp *ui;
-    long map(long x, long in_min, long in_max, long out_min, long out_max);
     QMQTT::Client *client;
     QString mqtt_topic_base="/openqt/base";
     QString mqtt_topic_arm="/openqt/arm";
+    QString mqtt_topic_sub="/openqt/#";
 };
 
 #endif // FLASHLAMP_H
